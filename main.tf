5df3f469ea6eba8ad2dc9e2768001999f4d747e4
lo0kat <louis.chomel@gmail.com>
2023-08-06 17:20:25 +0200

    Fix URL

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "ovh" {
 }
 
 # module "oks" {
-#   source       = "https://github.com/lo0kat/terraform-oks-module.git"
+#   source       = "github.com/lo0kat/terraform-oks-module.git"
 #   project_id   = ovh_cloud_project.my_cloud_project.id
 #   cluster_name = var.cluster_name
 #   region       = var.region
